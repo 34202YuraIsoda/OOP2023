008e87026866d2e1988c73d17c7daece13e41896
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-05-17 11:22:17 +0900

    P79 問題2.1完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //2.1.1
+    class Song {
+
+        //自動実装プロパティ
+        public string Title { get; set; }//歌のタイトル
+        public string ArtistName { get; set; }//アーティスト名
+        public int Length { get; set; }//演奏時間、単位は秒
+
+        //2.1.2
+        public Song(string title,string artistName,int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
+    }
+}
