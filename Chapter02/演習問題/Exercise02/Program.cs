ece90506497f0daf25a18c34fafb1796f4728ace
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-05-22 09:30:08 +0900

    P79　問題2.2完成

---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            if(args.Length < 3) {
+                return;
+            }
+            if (args[0] == "-tom") {
+                PrintInchToMeterList(int.Parse(args[1]), int.Parse(args[2]));
+            } else if(args[0] == "-toi") {
+                PrintMeterToInchList(int.Parse(args[1]), int.Parse(args[2]));
+            }
+        }
+
+        // フィートからメートルへの対応表を出力
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} in = {1:0.0000} m", inch, meter);
+            }
+        }
+
+        // メートルからフィートへの対応表を出力
+        private static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} in", meter, inch);
+            }
+        }
+
+    }
+}
+
