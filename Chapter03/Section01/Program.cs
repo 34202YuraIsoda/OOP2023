fb8d5d77bc27ba4286178eff609ec5433d6531ba
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-05-23 10:31:21 +0900

    P87まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,11 +8,14 @@ namespace Section01 {
     class Program {
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, 25, 11, 94, 60, 12, 64, 75, 32, 48, 95, 24, 16, 35, 48 };
 
-            int count = numbers.Count(n => n % 2 == 0);
+            //3以上8未満
+            //int count = numbers.Count(n => n % 5 == 0 && n > 0);
+            var sum = numbers.Where(n => n % 2 == 0).Average();
 
-            Console.WriteLine(count);
+
+            Console.WriteLine(sum);
 
         }
     }
