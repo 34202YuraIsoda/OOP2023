3ee20eade347820456b2a6588c02a34293a2fb5b
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-05-23 10:36:29 +0900

    P88から（新規作成）

---
+++ Chapter03/Section03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03 {
+    class Program {
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+        }
+    }
+}
