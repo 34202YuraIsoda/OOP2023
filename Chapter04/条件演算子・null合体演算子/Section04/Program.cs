cc6b148ab8cb7feaa1cccf5f0633ae6228c2117f
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-05-29 11:27:48 +0900

    null条件演算子

--- Chapter04/条件演算子・null合体演算子/Section04/Program.cs
+++ Chapter04/条件演算子・null合体演算子/Section04/Program.cs
@@ -19,9 +19,26 @@ namespace Section04 {
             #endregion
 
             #region null合体演算子
+#if false
             string code = "12345";
             var message = GetMessage(code) ?? DefaultMessage();
             Console.WriteLine(message);
+#endif
+            #endregion
+
+            #region null条件演算子
+#if true
+            //Sale sale = new Sale {
+            //    Amount = 100,
+            //};
+
+            Sale sale = null;
+
+            //「int?」はnull許容型、「?.」はnull条件演算子
+            int? ret = sale?.Amount;
+
+            Console.WriteLine(ret);
+#endif
             #endregion
 
         }
@@ -34,4 +51,15 @@ namespace Section04 {
             return "Default Message";
         }
     }
+
+    //売り上げクラス
+    public class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+        //商品カテゴリー
+        public string ProductCategory { get; set; }
+        //売上高
+        public int Amount { get; set; }
+    }
+
 }
