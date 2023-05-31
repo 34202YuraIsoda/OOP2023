7163fcedd310d2b1faa09553bee55c7ed028f991
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-05-31 11:42:55 +0900

    演習問題4.2完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -9,11 +9,62 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
 
-            var ym = new YearMonth(2023, 12);
-            var c21 = ym.Is21Century;
-            Console.WriteLine(ym);
-            Console.WriteLine(ym.AddOneMonth());
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2020, 12),
+            };
 
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
+
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
+
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
+
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var yearMonth in ymCollection) {
+                Console.WriteLine(yearMonth);
+            }
+        }
+
+        //4.2.3
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach (var yearMonth in yms) {
+                if (yearMonth.Is21Century) {
+                    return yearMonth;
+                }
+            }
+            return null;
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            var yearMonth = FindFirst21C(ymCollection);
+            if (yearMonth == null) {
+                Console.WriteLine("\"21世紀のデータはありません\"");
+            } else {
+                Console.WriteLine(yearMonth);
+            }
+            //Console.WriteLine(yearMonth == null ? "\"21世紀のデータはありません\"" : yearMonth.ToString());
+            //Console.WriteLine(yearMonth?.ToString() ?? "\"21世紀のデータはありません\"");
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            var yearMonths = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+            Exercise2_2(yearMonths);
+
+            //var array = ymCollection.Select(ym => ym.AddOneMonth()).OrderBy(ym => ym.Year).ToArray();
         }
     }
 }
