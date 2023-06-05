23e336001227651d367a97e8c8e68b4ad27808f4
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-06-05 10:59:30 +0900

    演習問題5.3完成

--- Chapter05/演習問題/Exercise01/Program.cs
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -25,43 +25,6 @@ namespace Exercise01 {
 #endif
             #endregion
 
-            #region 5.1.3
-#if true
-            var text = "Jackdaws love my big sphinx of quartz";
-
-            Exercise3_1(text);
-            Console.WriteLine("-----");
-
-            Exercise3_2(text);
-            Console.WriteLine("-----");
-
-            Exercise3_3(text);
-            Console.WriteLine("-----");
-
-            Exercise3_4(text);
-            Console.WriteLine("-----");
-
-            Exercise3_5(text);
-            {\rtf1}
-#endif
-            #endregion
-
-        }
-
-        private static void Exercise3_1(string text) {
-            text
-        }
-
-        private static void Exercise3_2(string text) {
-        }
-
-        private static void Exercise3_3(string text) {
-        }
-
-        private static void Exercise3_4(string text) {
-        }
-
-        private static void Exercise3_5(string text) {
         }
     }
 }
---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            #region 5.1.3
+#if true
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Exercise3_1(text);
+            Console.WriteLine("-----");
+
+            Exercise3_2(text);
+            Console.WriteLine("-----");
+
+            Exercise3_3(text);
+            Console.WriteLine("-----");
+
+            Exercise3_4(text);
+            Console.WriteLine("-----");
+
+            Exercise3_5(text);
+            //{\rtf1}
+#endif
+            #endregion
+
+        }
+
+        private static void Exercise3_1(string text) {
+            var count = text.Count(n => n.ToString().Contains(' '));
+            Console.WriteLine("空白：{0}", count);
+        }
+
+        private static void Exercise3_2(string text) {
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
+        }
+
+        private static void Exercise3_3(string text) {
+            var texts = text.Split(' ');
+            Console.WriteLine(texts.Length);
+        }
+
+        private static void Exercise3_4(string text) {
+            var texts = text.Split(' ').ToList();
+            texts.FindAll(s => s.Length <= 4).ForEach(n => Console.WriteLine(n));
+        }
+
+        private static void Exercise3_5(string text) {
+            var sb = new StringBuilder();
+            var texts = text.Split(' ');
+            foreach (var target in texts) {
+                sb.Append(target);
+                sb.Append(' ');
+            }
+            var connectionText = sb.ToString();
+            Console.WriteLine(connectionText);
+        }
+
+    }
+}
+
