80af810b1089ee0af54d53e8a0e40faf2d2f2039
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-06-05 12:02:15 +0900

    演習問題5.3解説後

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -31,8 +31,10 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_1(string text) {
-            var count = text.Count(n => n.ToString().Contains(' '));
+            var count = text.Count(c => c.ToString().Contains(' '));
             Console.WriteLine("空白：{0}", count);
+
+            //var count = text.Count(c => c == ' ');模範解答
         }
 
         private static void Exercise3_2(string text) {
@@ -42,23 +44,40 @@ namespace Exercise03 {
 
         private static void Exercise3_3(string text) {
             var texts = text.Split(' ');
-            Console.WriteLine(texts.Length);
+            Console.WriteLine("単語数：{0}", texts.Length);
+
+            //int count = text.Split(' ').Length;模範解答
         }
 
         private static void Exercise3_4(string text) {
-            var texts = text.Split(' ').ToList();
-            texts.FindAll(s => s.Length <= 4).ForEach(n => Console.WriteLine(n));
+            var words = text.Split(' ').ToList();
+            words.FindAll(s => s.Length <= 4).ForEach(s => Console.WriteLine(s));
+
+            //var words = text.Split(' ').Where(word => word.Length <= 4);模範解答
+            //foreach (var word in words) {
+            //    Console.WriteLine(word);
+            //}
         }
 
         private static void Exercise3_5(string text) {
             var sb = new StringBuilder();
-            var texts = text.Split(' ');
-            foreach (var target in texts) {
-                sb.Append(target);
+            var words = text.Split(' ');
+            foreach (var word in words) {
+                sb.Append(word);
                 sb.Append(' ');
             }
-            var connectionText = sb.ToString();
-            Console.WriteLine(connectionText);
+            sb.Remove(sb.Length - 1, 1);
+            var connectedText = sb.ToString();
+            Console.WriteLine(connectedText);
+
+            //var array = text.Split(' ').ToArray();
+            //var sb = new StringBuilder(array[0]);
+            //foreach (var word in array.Skip(1)) {
+            //sb.Append(' ');
+            //sb.Append(word);
+            //}
+            //var createWords = sb.ToString();
+            //Console.WriteLine(createWords);模範解答
         }
 
     }
