cd3686513601cd0eca25d4dfb7658cd4e0177fad
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-06-07 11:51:06 +0900

    section1.2

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4 };
+            Console.WriteLine(numbers.Average());
+
+            var books = Books.GetBooks();
+
+            Console.WriteLine(books.Average(x => x.Price));
+            Console.WriteLine(books.Sum(x => x.Pages));
+            Console.WriteLine(books.Max(x => x.Price));
+            var booksObj = books.Where(x => x.Title.Contains("物語")).OrderByDescending(x => x.Price);
+            foreach (var book in booksObj) {
+                Console.WriteLine("{0}:{1}円", book.Title, book.Price);
+            }
+        }
+    }
+}
