23b8efd97ba6d226b389e16661ea4ad0f5bf5ba8
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-06-19 15:56:23 +0900

    設問２_提出

--- TestProject/Test02/Program.cs
+++ TestProject/Test02/Program.cs
@@ -37,7 +37,8 @@ namespace Test02 {
         //　　　　出力結果【618】
         private static void Exercise01(List<int> numbers) {
 
-
+            var number = numbers.Sum();
+            Console.WriteLine("{0}", number);
 
         }
 
@@ -45,14 +46,18 @@ namespace Test02 {
         //　　　　出力結果【94】
         private static void Exercise02(List<int> numbers) {
 
-
+            var number = numbers.Where(n => n % 2 == 0).Max();
+            Console.WriteLine("{0}", number);
 
         }
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(List<int> numbers) {
 
-
+            var number = numbers.OrderBy(n => n);
+            foreach (var num in number) {
+                Console.Write("{0} ", num);
+            }
 
         }
 
@@ -60,7 +65,10 @@ namespace Test02 {
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(List<int> numbers) {
 
-
+            var number = numbers.Where(n => 10 <= n && n <= 50);
+            foreach (var num in number) {
+                Console.Write("{0} ", num);
+            }
 
         }
     }
