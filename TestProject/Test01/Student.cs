c6f1885a56ba1761109bc650db8f8a6450990b7e
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-06-19 14:56:42 +0900

    設問1_Studentクラス完成

--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,7 +1,10 @@
 ﻿namespace Test01 {
     class Student {
 
-
+        //プロパティ
+        public string Name { get; set; }//学生の名前
+        public string Subject { get; set; }//科目名
+        public int Score { get; set; }//点数
 
     }
 }
