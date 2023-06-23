c4d8e4e83630c69c49fedd940e908f53042af4b8
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-06-23 11:49:46 +0900

    Dictionaryを使った県庁所在地登録プログラム

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -9,34 +9,63 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            string prefectureName, cityName;
+            string prefectureName, cityName, yORn;
+            int population;
             var cultureInfo = new CultureInfo("ja-JP");
-            var prefectureDict = new Dictionary<string, string>();
+            var prefectureDict = new Dictionary<string, CityInfo>();
 
             Console.WriteLine("県庁所在地の登録");
+
             Console.Write("県名：");
-            prefectureName = Console.ReadLine();
+            prefectureName = Console.ReadLine();//県名の入力
+
             while (string.Compare(prefectureName, "999", cultureInfo, CompareOptions.IgnoreWidth) != 0) {
+
                 Console.Write("所在地：");
-                cityName = Console.ReadLine();
+                cityName = Console.ReadLine();//所在地の入力
+
                 if (prefectureDict.ContainsKey(prefectureName)) {
-                    Console.WriteLine("{0}の県庁所在地は{1}として登録済みです。上書きしますか？ y/n", prefectureName, prefectureDict[prefectureName]);
-                    if (string.Compare(Console.ReadLine(), "y", cultureInfo, CompareOptions.IgnoreWidth) == 0) {
-                        prefectureDict[prefectureName] = cityName;//上書き
+                    Console.Write("{0}の所在地は{1}として登録済みです。上書きしますか？ (y/n)：", prefectureName, prefectureDict[prefectureName].City);
+                    while (true) {
+                        yORn = Console.ReadLine();
+                        if (string.Compare(yORn, "y", cultureInfo, CompareOptions.IgnoreWidth) == 0) {
+                            prefectureDict[prefectureName].City = cityName;//上書き
+                            Console.WriteLine("上書きしました");
+                            break;
+                        } else if (string.Compare(yORn, "n", cultureInfo, CompareOptions.IgnoreWidth) == 0) {
+                            Console.WriteLine("キャンセルしました");//キャンセル
+                            break;
+                        } else {
+                            Console.WriteLine("既定の文字以外が入力されました。再入力してください。");//yとn以外が入力されたとき
+                            continue;
+                        }
                     }
-                } else {
-                    prefectureDict[prefectureName] = cityName;//新規登録
                 }
+
+                Console.Write("人口：");
+                population = int.Parse(Console.ReadLine());//人口の入力
+
+                //登録処理
+                prefectureDict[prefectureName] = new CityInfo {
+                    City = cityName,
+                    Population = population
+                };
+
                 Console.Write("県名：");
-                prefectureName = Console.ReadLine();
+                prefectureName = Console.ReadLine();//県名の入力
+
             }
 
             Console.WriteLine("1：一覧表示,2：県名指定");
+            Console.Write(">");
             switch (Console.ReadLine()) {
 
                 case "1":
-                    foreach (var prefecture in prefectureDict) {
-                        Console.WriteLine("{0}({1})", prefecture.Key, prefecture.Value);
+                    if (prefectureDict.Count != 0) {
+                        var prefectureDictDesc = prefectureDict.OrderByDescending(p=>p.Value.Population);
+                        foreach (var prefecture in prefectureDictDesc) {
+                            Console.WriteLine("{0}【 {1} (人口：{2:N0}人)】", prefecture.Key, prefecture.Value.City, prefecture.Value.Population);
+                        }
                     }
                     break;
 
@@ -44,7 +73,7 @@ namespace Section01 {
                     Console.Write("県名を入力：");
                     var search = Console.ReadLine();
                     if (prefectureDict.ContainsKey(search)) {
-                        Console.WriteLine("{0}({1})", search, prefectureDict[search]);
+                        Console.WriteLine("{0}【 {1} ( 人口：{2:N0}人)】", search, prefectureDict[search].City, prefectureDict[search].Population);
                     }
                     break;
 
@@ -55,7 +84,7 @@ namespace Section01 {
     }
 
     class CityInfo {
-        string City { get; set; }
-        string Population { get; set; }
+        public string City { get; set; }//都市
+        public int Population { get; set; }//人口
     }
 }
