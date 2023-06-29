4ff68df7a7e090c7034f7d6b206ce94b100d3f0c
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-06-29 11:29:59 +0900

    演習問題7.1完成

--- Chapter07/演習問題/Exercise01/Program.cs
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -7,6 +7,47 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
+
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);
+            Console.WriteLine();
+            Exercise1_2(text);
+
+        }
+
+        private static void Exercise1_1(string text) {
+            var dict = new Dictionary<char, int>();
+            foreach (var c in text.ToUpper()) {
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey(c)) {
+                        dict[c]++;
+                    } else {
+                        dict.Add(c, 1);
+                    }
+                }
+            }
+            foreach (var item in dict.OrderBy(d => d.Key)) {
+                Console.WriteLine("'{0}'：{1}", item.Key, item.Value);
+            }
+
+        }
+
+        private static void Exercise1_2(string text) {
+            var dict = new SortedDictionary<char, int>();
+            foreach (var c in text.ToUpper()) {
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey(c)) {
+                        dict[c]++;
+                    } else {
+                        dict.Add(c, 1);
+                    }
+                }
+            }
+            foreach (var item in dict) {
+                Console.WriteLine("'{0}'：{1}", item.Key, item.Value);
+            }
+
+
         }
     }
 }
