a53fde171a14fd7c243c56da9689a03b43c07864
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-06-29 12:09:03 +0900

    演習問題7.2途中

--- Chapter07/演習問題/Exercise02/Program.cs
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+
+            // コンストラクタ呼び出し
+            var abbrs = new Abbreviations();
+
+            // Addメソッドの呼び出し
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+
+            // 7.2.3
+            Console.WriteLine("用語の数：{0}", abbrs.Count);
+            abbrs.remove("IOC");
+            Console.WriteLine("用語の数：{0}", abbrs.Count);
+
+            // 7.2.4
+            // IEumerable<> を実装したので、LINQが使える
+            abbrs.
+
         }
     }
 }
