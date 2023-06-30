d5fe8c8b516326a9779d31124530b824b695ddec
磯田 由樂 <OIBC34202@ojs.ac.jp>
2023-06-30 12:09:36 +0900

    演習問題8.1完成　8.2途中

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            var dateTime = new DateTime(2019, 1, 15, 19, 48, 32);
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+            DisplayDatePattern3_2(dateTime);
+
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            var str = dateTime.ToString("yyyy/MM/dd HH:mm");
+            Console.WriteLine(str);
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            var str = dateTime.ToString("yyyy年MM月dd日 HH時mm分ss秒");
+            Console.WriteLine(str);
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = dateTime.ToString("ggyy年 M月dd日(dddd)",culture);
+            Console.WriteLine(str);
+        }
+
+        private static void DisplayDatePattern3_2(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var dateStr = dateTime.ToString("ggyy年MM月dd日(dddd)", culture);
+            var str = Regex.Replace(dateStr, @"0(\d)", "$1");
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            DateTime dateTime = DateTime.Today;
+
+            foreach (var dayOfWeek in Enum.GetValues(typeof(DayOfWeek))) {
+                Console.WriteLine(NextDay(dateTime,(DayOfWeek)dayOfWeek).ToString("yyyy年MM月dd日(dddd)"));
+            }
+        }
+
+        public static DateTime NextDay(DateTime date, DayOfWeek dayOfWeek) {
+            var days = (int)dayOfWeek - (int)(date.DayOfWeek) + 7;
+            return date.AddDays(days);
+        }
+    }
+}
