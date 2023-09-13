bd4abac8b75cb9d52bcab955ffecda77e2b2684e
磯田　由樂 <OIBC34202@ojs.ac.jp>
2023-09-13 09:24:53 +0900

    BooksテーブルとAuthorsテーブルをcodefirstで作成

---
+++ Chapter13/SampleEntityFramework/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEntityFramework.Models {
+    public class Author {
+        public int Id { get; set; } //主キー
+        public string Name { get; set; } //著者名
+        public DateTime Birthday { get; set; } //誕生日
+        public string Gender { get; set; } //性別
+        public virtual ICollection<Book> Books { get; set; } //本
+    }
+}
---
+++ Chapter13/SampleEntityFramework/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEntityFramework.Models {
+    public class Book {
+        public int Id { get; set; } //主キー
+        public string Title { get; set; } //本のタイトル
+        public int PublishedYear { get; set; } //出版年
+        public virtual Author Author { get; set; } //著者
+    }
+}
