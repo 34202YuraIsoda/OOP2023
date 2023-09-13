96806da3d4013075def44a1bccf1da293b8851b6
磯田　由樂 <OIBC34202@ojs.ac.jp>
2023-09-13 11:09:27 +0900

    データ注釈

--- Chapter13/SampleEntityFramework/Models/Author.cs
+++ Chapter13/SampleEntityFramework/Models/Author.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleEntityFramework.Models {
     public class Author {
         public int Id { get; set; } //主キー
+        [MaxLength(30)]
+        [Required]
         public string Name { get; set; } //著者名
         public DateTime Birthday { get; set; } //誕生日
         public string Gender { get; set; } //性別
