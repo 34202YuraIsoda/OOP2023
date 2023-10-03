984bcdc57ae9f59c862903febc491640fece7cb4
磯田　由樂 <OIBC34202@ojs.ac.jp>
2023-10-03 11:13:02 +0900

    テキストボックスに入力したurlのニュースの一覧表示・それぞれ選択したwebサイト表示完成 登録処理途中

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -27,6 +27,9 @@ namespace RssReader {
             this.tbUrl = new System.Windows.Forms.TextBox();
             this.btGet = new System.Windows.Forms.Button();
             this.lbRssTitle = new System.Windows.Forms.ListBox();
+            this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // tbUrl
@@ -53,18 +56,52 @@ namespace RssReader {
             this.lbRssTitle.ItemHeight = 12;
             this.lbRssTitle.Location = new System.Drawing.Point(13, 51);
             this.lbRssTitle.Name = "lbRssTitle";
-            this.lbRssTitle.Size = new System.Drawing.Size(773, 580);
+            this.lbRssTitle.Size = new System.Drawing.Size(871, 112);
             this.lbRssTitle.TabIndex = 2;
+            this.lbRssTitle.Click += new System.EventHandler(this.lbRssTitle_Click);
+            // 
+            // wbBrowser
+            // 
+            this.wbBrowser.Location = new System.Drawing.Point(13, 170);
+            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbBrowser.Name = "wbBrowser";
+            this.wbBrowser.ScriptErrorsSuppressed = true;
+            this.wbBrowser.Size = new System.Drawing.Size(871, 464);
+            this.wbBrowser.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(792, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(13, 640);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(871, 40);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(798, 646);
+            this.ClientSize = new System.Drawing.Size(893, 690);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.wbBrowser);
             this.Controls.Add(this.lbRssTitle);
             this.Controls.Add(this.btGet);
             this.Controls.Add(this.tbUrl);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -76,6 +113,9 @@ namespace RssReader {
         private System.Windows.Forms.TextBox tbUrl;
         private System.Windows.Forms.Button btGet;
         private System.Windows.Forms.ListBox lbRssTitle;
+        private System.Windows.Forms.WebBrowser wbBrowser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -12,18 +12,56 @@ using System.Xml.Linq;
 
 namespace RssReader {
     public partial class Form1 : Form {
+        List<ItemData> itemDatas = new List<ItemData>();
+
         public Form1() {
             InitializeComponent();
         }
 
         private void btGet_Click(object sender, EventArgs e) {
+            //https://news.yahoo.co.jp/rss/topics/it.xml
+            if (tbUrl.Text.Contains("xml")) {
+                using (var wc = new WebClient()) {
+                    var url = wc.OpenRead(tbUrl.Text);
+                    XDocument xdoc = XDocument.Load(url);
+                    itemDatas = xdoc.Root.Descendants("item").Select(x => new ItemData {
+                        Title = (string)x.Element("title"),
+                        Link = (string)x.Element("link")
+                    }).ToList();
+                    foreach (var node in itemDatas) {
+                        lbRssTitle.Items.Add(node.Title);
+                    }
+                }
+            }
+        }
+
+        private void lbRssTitle_Click(object sender, EventArgs e) {
+            if (lbRssTitle.SelectedIndex != -1) {
+                var d = itemDatas.First(x => x.Title.Equals(lbRssTitle.SelectedItem));
+                wbBrowser.Navigate(d.Link);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
             using (var wc = new WebClient()) {
                 var url = wc.OpenRead(tbUrl.Text);
                 XDocument xdoc = XDocument.Load(url);
-                var nodes = xdoc.Root.Descendants("item").Elements("title");
-                foreach (var node in nodes) {
-                    lbRssTitle.Items.Add(node.Value);
+                var channelDatas = xdoc.Root.Descendants("channel").Select(x => new ItemData {
+                    Title = (string)x.Element("title"),
+                    Link = (string)x.Element("link")
+                }).ToList();
+                foreach (var node in channelDatas) {
+                    listBox1.Items.Add(node.Title);
                 }
+            }
+            if (!listBox1.Items.Contains(tbUrl.Text) && tbUrl.Text.Trim() != "") {
+                listBox1.Items.Add(tbUrl.Text);
+            }
+        }
+
+        private void listBox1_Click(object sender, EventArgs e) {
+            if (listBox1.SelectedIndex != -1) {
+                tbUrl.Text = (string)listBox1.Items[listBox1.SelectedIndex];
             }
         }
     }
