fb5f903466f2415d7086af9621b7a46133950e51
磯田　由樂 <OIBC34202@ojs.ac.jp>
2023-10-04 12:07:05 +0900

    Exercise01完成

---
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(IsPhoneNumber("090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("080-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("060-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("190-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("091-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-12341"));
+            Console.WriteLine(IsPhoneNumber("A090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-911-1234"));
+            Console.WriteLine(IsPhoneNumber("090-1911-234"));
+        }
+
+        private static bool IsPhoneNumber(string str) {
+            return Regex.IsMatch(str,@"^(0[7-9]0)-(\d{4})-(\d{4})$");
+
+        }
+    }
+}
