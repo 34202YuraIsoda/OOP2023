e0de3c6584a64b5d345393ec2be41dad8d291d2b
磯田　由樂 <OIBC34202@ojs.ac.jp>
2023-10-05 11:00:38 +0900

    Exercise03完成

---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
+            foreach (var text in texts) {
+                var strs = Regex.Matches(text, @"\btime\b", RegexOptions.IgnoreCase);
+                foreach (Match str in strs) {
+                    if (str.Success) {
+                        Console.WriteLine("{0}:{1}", text, str.Index);
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
