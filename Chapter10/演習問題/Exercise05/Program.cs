946cc2c994a99ac3be03974f24b1b038a6f33903
磯田　由樂 <OIBC34202@ojs.ac.jp>
2023-10-05 12:08:54 +0900

    Exercise05途中

---
+++ Chapter10/演習問題/Exercise05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Exercise05 {
+    class Program {
+        static void Main(string[] args) {
+            TagLower("sample.html");
+        }
+
+        private static void TagLower(string file) {
+            var lines = File.ReadLines(file);
+            var sb = new StringBuilder();
+            foreach (var line in lines) {
+                var matches = Regex.Matches(line, @"<.+[^<>]>\b");
+                foreach (Match match in matches) {
+                    Console.WriteLine(match);
+                }
+            }
+
+
+
+
+
+
+
+            //ファイル出力
+            //File.WriteAllText(file, sb.ToString());
+        }
+    }
+}
