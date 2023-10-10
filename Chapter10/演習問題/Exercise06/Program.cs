cfb07763bd580c2d5bd7765ba5da21e38dda0c8e
磯田　由樂 <OIBC34202@ojs.ac.jp>
2023-10-10 11:47:29 +0900

    Exercise06完成

---
+++ Chapter10/演習問題/Exercise06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise06 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "しるし しんぶんし きたのたき きもの いろしろい トマト ヨクナクヨ　あああああ　あいういあ　あいうえお　あいうえあ　ppppp　paeap　abcde　12345　11111　12321　[:-:[　[[[[[　:;@;:";
+
+            var pattern = @"\b([\p{IsKatakana}\p{IsHiragana}])([\p{IsKatakana}\p{IsHiragana}])\w\2\1\b"; //←ここにパターンを記述
+            var matches = Regex.Matches(text, pattern);
+            foreach (Match m in matches)
+                Console.WriteLine("'{0}'", m.Value);
+        }
+    }
+}
