df905cc9ae9838614bae31c28997fcd8113de690
磯田　由樂 <OIBC34202@ojs.ac.jp>
2023-10-10 11:53:49 +0900

    Exercise04まで自力ででき、Exercise05,06は答えを聞きながら

--- Chapter10/演習問題/Exercise06/Program.cs
+++ Chapter10/演習問題/Exercise06/Program.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 namespace Exercise06 {
     class Program {
         static void Main(string[] args) {
-            var text = "しるし しんぶんし きたのたき きもの いろしろい トマト ヨクナクヨ　あああああ　あいういあ　あいうえお　あいうえあ　ppppp　paeap　abcde　12345　11111　12321　[:-:[　[[[[[　:;@;:";
+            var text = "しるし しんぶんし きたのたき きもの いろしろい トマト ヨクナクヨ　あああああ　あいういあ　あいうえお　あいうえあ　ppppp　paeap　abcde　12345　11111　12321　[:-:[　[[[[[　:;@;:　あいpいあ";
 
-            var pattern = @"\b([\p{IsKatakana}\p{IsHiragana}])([\p{IsKatakana}\p{IsHiragana}])\w\2\1\b"; //←ここにパターンを記述
+            var pattern = @"\b([\p{IsKatakana}\p{IsHiragana}])([\p{IsKatakana}\p{IsHiragana}])[\p{IsKatakana}\p{IsHiragana}]\2\1\b"; //←ここにパターンを記述
             var matches = Regex.Matches(text, pattern);
             foreach (Match m in matches)
                 Console.WriteLine("'{0}'", m.Value);
